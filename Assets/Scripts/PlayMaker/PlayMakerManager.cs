7e4f753b67b4007bf8b72e6917908dc05f68dd27
paulalom <paul.lomenda@gmail.com>
2017-07-23 16:56:27 -0400

    Added PlayMaker Scene
    Set up basics for new planning scene.
    Has a resizable court and draggable UI.

---
+++ Assets/Scripts/PlayMaker/PlayMakerManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayMakerManager : MonoBehaviour {
+    
+    public PlayMakerScreenManager screenManager;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlayMaker/Toolbar/PlayMakerToolbar.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayMakerToolbar : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
